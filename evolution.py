453ae57e13b10ca52725a40acf826bd4f363dfa2
S. Jacob Powell <s.jacob.powell@gmail.com>
2017-02-25 13:50:06 -0700

    Fit the files together

--- evolution.py
+++ evolution.py
@@ -2,10 +2,14 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, BatchNormalization
 from keras.layers import Dense, Dropout, Activation
+from genome_handler import GenomeHandler
+
 
 # Our genetic algorithm.
 # This will contain methods for fitness, crossover, mutation, etc.
 class Evolutions:
+    def __init__():
+        self.genome_handler = GenomeHandler()
 
     # Run the whole thing
     def run(self):
---
+++ main.py
@@ -0,0 +1,9 @@
+from evolution import Evolutions
+
+
+def main():
+    Evolutions().run()
+
+
+if __name__ == '__main__':
+    main()
