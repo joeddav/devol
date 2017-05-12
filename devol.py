285acebd53534e25a94b1f8b95101715126609d8
Joseph Davison <josephddavison@gmail.com>
2017-05-12 00:28:40 -0400

    added stdouts, misc

--- devol.py
+++ devol.py
@@ -15,16 +15,18 @@ class Devol:
         self.genome_handler = genome_handler
         self.datafile = data_path or datetime.now().ctime() + '.csv'
         print "Genome encoding and accuracy data stored at", self.datafile
-        self.data = []
 
     # Create a population and evolve
     def run(self, dataset, num_generations, pop_size, epochs):
         print "Initial Population..."
         (self.x_train, self.y_train), (self.x_test, self.y_test) = dataset
+
         # Generate initial random population
-        members = np.array([self.genome_handler.generate() for _ in range(pop_size)])
-        fit = np.array(self.fitnesses(members, epochs))
+        members = [self.genome_handler.generate() for _ in range(pop_size)]
+        fit = [self.evaluate(member, epochs)[1] for member in members]
         pop = Population(members, fit)
+        fit = np.array(fit)
+        print "max:", max(fit), "\taverage:", np.mean(fit), "\tstd:", np.std(fit)
 
         # Evolve over generations
         for gen in range(1, num_generations):
@@ -35,18 +37,10 @@ class Devol:
             members += pop.getBest(pop_size - int(pop_size*0.95))
             for i in range(len(members)): # Mutation
                 members[i] = self.mutate(members[i], gen)
-            fit = np.array(self.fitnesses(members, epochs))
-            members = np.array(members)
+            fit = [self.evaluate(member, epochs)[1] for member in members]
             pop = Population(members, fit)
-
-    def fitnesses(self, genomes, epochs):
-        accuracies = [self.evaluate(x, epochs)[1] for x in genomes]	
-        accuracies -= min(accuracies)
-        accuracies /= max(accuracies)
-        return map(lambda x: self.fitness(x), accuracies)
-
-    def fitness(self, val):
-        return (val * 100) ** 4
+            fit = np.array(fit)
+            print "max:", max(fit), "\taverage:", np.mean(fit), "\tstd:", np.std(fit)
 
     # Returns the accuracy for a model as 1 / loss
     def evaluate(self, genome, epochs):
@@ -61,8 +55,8 @@ class Devol:
         #     callbacks=[EarlyStopping(monitor='val_loss', patience=2, verbose=1)])
         model.fit(self.x_train, self.y_train, validation_data=(self.x_test, self.y_test),
             epochs=epochs,
-            verbose=1,
-            callbacks=[EarlyStopping(monitor='val_loss', patience=1, verbose=1)])
+            verbose=0,
+            callbacks=[EarlyStopping(monitor='val_loss', patience=1, verbose=0)])
         loss, accuracy = model.evaluate(self.x_test, self.y_test, verbose=0)
 
         # Record the stats
@@ -74,13 +68,9 @@ class Devol:
         return loss, accuracy
     
     def crossover(self, genome1, genome2):
-        genome1 = genome1.tolist()
-        genome2 = genome2.tolist()
-        #swap the genomes split at the crossover index
         crossIndexA = rand.randint(0, len(genome1))
         child = genome1[:crossIndexA] + genome2[crossIndexA:]
-        
-        return np.array(child)
+        return child
     
     def mutate(self, genome, generation):
         num_mutations = max(3, generation / 2)
@@ -93,11 +83,16 @@ class Population:
 
     def __init__(self, members, fitnesses):
         self.members = members
-        self.fitnesses = fitnesses
-        self.s_fit = sum(self.fitnesses)
+        fitnesses -= min(fitnesses)
+        fitnesses /= max(fitnesses)
+        self.scores = map(self.score, fitnesses)
+        self.s_fit = sum(self.scores)
+
+    def score(self, fitness):
+        return (fitness * 100)**4
 
     def getBest(self, n):
-        combined = [(self.members[i], self.fitnesses[i]) \
+        combined = [(self.members[i], self.scores[i]) \
                 for i in range(len(self.members))]
         sorted(combined, key=(lambda x: x[1]), reverse=True)
         return map(lambda x: x[0], combined[:n])
@@ -106,6 +101,6 @@ class Population:
         dart = rand.uniform(0, self.s_fit)
         sum_fits = 0
         for i in range(len(self.members)):
-            sum_fits += self.fitnesses[i]
+            sum_fits += self.scores[i]
             if sum_fits > dart:
                 return self.members[i]
